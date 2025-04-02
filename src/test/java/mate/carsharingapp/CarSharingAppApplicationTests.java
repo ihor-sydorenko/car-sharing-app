00b08d52df41bc5408c1f1c908d0dc90f40e815a
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2025-04-02 12:18:11 +0300

    project configuration

--- src/test/java/mate/carsharingapp/CarSharingAppApplicationTests.java
+++ src/test/java/mate/carsharingapp/CarSharingAppApplicationTests.java
@@ -1,13 +1,8 @@
 package mate.carsharingapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarSharingAppApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
